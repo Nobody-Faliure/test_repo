5044532f8efd383b55073cf555d3d75ebaf1d2f2
Yuan Sun <yuansun@Yuans-MacBook-Air.local>
2025-11-10 13:47:28 -0800

    Rickroll Function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 class Annoy:
     def __init__(self):
         self._annoy : bool = False
@@ -15,3 +17,18 @@ class Annoy:
     def set_annoy_level(self, new_index: int):
         if self._annoy:
             self._annoy_index = new_index
+            if self._annoy_index == 0:
+                self._annoy_index = 5
+
+    def rickroll(self):
+        if self._annoy:
+            if self._annoy_index <= 3:
+                for i in range(4 ^ self._annoy_index):
+                    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9uXgQ")
+            if self._annoy_index == 4:
+                for i in range(200):
+                    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9uXgQ")
+            else:
+                while True:
+                    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9uXgQ")
+    
