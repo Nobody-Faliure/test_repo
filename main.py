384799b460136fa23c668af884ac539cf395ebbc
Yuan Sun <yuansun@Yuans-MacBook-Air.local>
2025-11-10 13:52:05 -0800

    Golden Fucntion

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 class Annoy:
     def __init__(self):
         self._annoy : bool = False
@@ -15,3 +17,15 @@ class Annoy:
     def set_annoy_level(self, new_index: int):
         if self._annoy:
             self._annoy_index = new_index
+
+    def golden(self):
+        if self._annoy:
+            if self._annoy_index <= 3:
+                for i in range(4 ^ self._annoy_index):
+                    webbrowser.open("https://www.youtube.com/watch?v=yebNIHKAC4A&list=RDyebNIHKAC4A&start_radio=1")
+            if self._annoy_index == 4:
+                for i in range(200):
+                    webbrowser.open("https://www.youtube.com/watch?v=yebNIHKAC4A&list=RDyebNIHKAC4A&start_radio=1")
+            else:
+                while True:
+                    webbrowser.open("https://www.youtube.com/watch?v=yebNIHKAC4A&list=RDyebNIHKAC4A&start_radio=1")
