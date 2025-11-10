3cb878c0b595ed67a2552f8193a97a62cd6d3afd
Yuan Sun <yuansun@Yuans-MacBook-Air.local>
2025-11-10 13:30:35 -0800

    Intial Annoying Functions

---
+++ main.py
@@ -0,0 +1,17 @@
+class Annoy:
+    def __init__(self):
+        self._annoy : bool = False
+        self._annoy_index: int = 0
+        print("Warning: This module can glitch and force your computer to restart")
+        print("Warning: Not recommended to set annoy_level > 3")
+
+    def init(self):
+        self._annoy = True
+
+    def uninit(self):
+        self._annoy = False
+        self._annoy_index = 0
+
+    def set_annoy_level(self, new_index: int):
+        if self._annoy:
+            self._annoy_index = new_index
